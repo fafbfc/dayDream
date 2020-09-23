fa36b7fb2de781426a8e49f3b1df29871345b511
fafbfc <1352239419@qq.com>
2020-09-23 11:34:51 +0800

    初始化项目

--- src/main/java/cn/edu/gdsdxy/sanlingerproject/SanlingerprojectApplication.java
+++ src/main/java/cn/edu/gdsdxy/sanlingerproject/SanlingerprojectApplication.java
@@ -1,5 +1,7 @@
 package cn.edu.gdsdxy.sanlingerproject;
 
+import lombok.Data;
+import lombok.Setter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
